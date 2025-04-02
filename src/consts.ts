8485f50ee997b1ca8d1b6980fce6a80ae49588ca
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-04-02 06:17:44 -0700

    this week about to go crazy

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'AP World Unit 9 | Globalization';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
