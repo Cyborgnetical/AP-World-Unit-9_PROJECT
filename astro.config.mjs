e017a00472e88f69522bf0f3316c70a04ee880a8
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-04-03 23:25:58 +0000

    building

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://cyborgnetical.github.io',
+	base:"/AP-World-Unit-9_PROJECT",
 	integrations: [mdx(), sitemap()],
 });
